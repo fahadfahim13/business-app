d4a5a9714d5b71985ee63c3a358b4d7bb3ef2943
fahadfahim13 <fahadfahim13@gmail.com>
2024-06-10 16:05:35 +0600

    Added comments

--- components/BusinessDetails/Reviews.tsx
+++ components/BusinessDetails/Reviews.tsx
@@ -1,20 +1,46 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import React, { useState } from "react";
 import { Rating } from "react-native-ratings";
 import { Colors, PRIMARY } from "@/constants/Colors";
+import { db } from "@/config/FirebaseConfig";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { useUser } from "@clerk/clerk-expo";
 
 const Reviews = (props: { business: any }) => {
   const { business } = props;
+  const { user } = useUser();
   const [rating, setRating] = useState("4.5");
   const [review, setReview] = useState("");
   const ratingCompleted = (r: string) => {
     setRating(r);
   };
+
+  const onSubmit = async () => {
+    const docRef = doc(db, "BusinessList", business?.id ?? "");
+    await updateDoc(docRef, {
+      reviews: arrayUnion({
+        rating: rating,
+        comment: review,
+        userId: user?.id,
+        useFullname: user?.fullName,
+        userImage: user?.imageUrl,
+      }),
+    });
+    ToastAndroid.show('Review Added Successfully!!', ToastAndroid.TOP);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Reviews</Text>
       <Rating
-      imageSize={24}
+        imageSize={24}
         showRating={false}
         onFinishRating={ratingCompleted}
         style={{ paddingVertical: 10 }}
@@ -25,10 +51,14 @@ const Reviews = (props: { business: any }) => {
         style={styles.reviewText}
         onChangeText={(text: string) => setReview(text)}
       />
-      <TouchableOpacity style={{...styles.submitButton,
-        backgroundColor: review !== '' ? PRIMARY : '#A3A6FF',
-      }}
-      disabled={review === ''}>
+      <TouchableOpacity
+        style={{
+          ...styles.submitButton,
+          backgroundColor: review !== "" ? PRIMARY : "#A3A6FF",
+        }}
+        disabled={review === ""}
+        onPress={onSubmit}
+      >
         <Text style={styles.buttonText}>Submit</Text>
       </TouchableOpacity>
     </View>
@@ -56,12 +86,12 @@ const styles = StyleSheet.create({
   submitButton: {
     marginTop: 12,
     padding: 12,
-    borderRadius: 16
+    borderRadius: 16,
   },
   buttonText: {
-    color: '#FFF',
-    textAlign: 'center',
-    fontFamily: 'Inter',
+    color: "#FFF",
+    textAlign: "center",
+    fontFamily: "Inter",
     fontSize: 16,
-  }
+  },
 });
